0d94057013247a2474499bd1b7588294d4f95b41
dmitryweiner <dmitry.weiner@gmail.com>
2023-04-03 10:56:38 +0300

    Use clarifai API (3)

--- client/src/servises/api.ts
+++ client/src/servises/api.ts
@@ -109,7 +109,7 @@ export const API = {
   },
   clarifai: {
     detect: async (filename: string) => {
-      const data = {
+/*      const data = {
         "user_app_id": {
           "user_id": "clarifai",
           "app_id": "main"
@@ -133,7 +133,69 @@ export const API = {
             'Authorization': `Key ${CLARIFAI_KEY}`
           },
           baseURL: "https://api.clarifai.com"
-        });
+        });*/
+      const raw = JSON.stringify({
+
+        "user_app_id": {
+
+          "user_id": "clarifai",
+
+          "app_id": "main"
+
+        },
+
+        "inputs": [
+
+          {
+
+            "data": {
+
+              "image": {
+
+                "url": "https://samples.clarifai.com/metro-north.jpg"
+
+              }
+
+            }
+
+          }
+
+        ]
+
+      });
+
+
+      const requestOptions = {
+
+        method: 'POST',
+
+        headers: {
+
+          'Accept': 'application/json',
+
+          'Authorization': 'Key ' + 'f720bf21656a4d969448903cd99f2343'
+
+        },
+
+        body: raw
+
+      };
+
+
+      // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+
+      // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+
+      // this will default to the latest version_id
+
+
+      fetch(`https://api.clarifai.com/v2/models/general-image-recognition/versions/aa7f35c01e0642fda5cf400f543e7c40/outputs`, requestOptions)
+
+        .then(response => response.text())
+
+        .then(result => console.log(result))
+
+        .catch(error => console.log('error', error));
     }
   }
 
